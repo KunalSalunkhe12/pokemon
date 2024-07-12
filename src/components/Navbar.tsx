57f7cada3fac99b7f6a3536f45e05edc3b5b7f1a
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-07-13 00:26:52 +0530

    feat: Add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="z-20 sticky top-0 right-0 px-6 md:px-12 py-5 bg-primary text-white text-3xl">
+      <Link to="/" className="font-bold">
+        Pokemon ⚡
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
